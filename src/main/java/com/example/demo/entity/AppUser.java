520b1a9f970fc5e92523ac53452ea7fb456a0fc7
bala-amypo <bala@amypo.in>
2025-12-18 08:50:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name = "app_users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Table(
+    // name = "app_users", 
+    uniqueConstraints = @UniqueConstraint(columnNames = "email")
+    )
 public class AppUser {
 
     @Id
