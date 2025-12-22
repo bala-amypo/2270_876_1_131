ee93566740c79299248508c3eb64d530966c3e01
bala-amypo <bala@amypo.in>
2025-12-22 08:07:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -1,73 +1,26 @@
 package com.example.demo.entity;
-import jakarta.persistence.*;
-import java.util.Set;
 
-// @Table(
-//     name = "app_users", 
-//     uniqueConstraints = @UniqueConstraint(columnNames = "email")
-//     )
+import jakarta.persistence.*;
+import lombok.*;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AppUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String email;
 
-    @Column(nullable = false)
     private String password;
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @Enumerated(EnumType.STRING)
-    private Set<Role> roles;
+    private String role;
 
-    private Boolean active = true;
-
-    public AppUser() {}
-
-    public AppUser(String email, String password, Set<Role> roles, Boolean active) {
-        this.email = email;
-        this.password = password;
-        this.roles = roles;
-        this.active = active;
-    }
-
-
-    public Long getId() { 
-        return id; 
-    }
-
-    public String getEmail() { 
-        return email; 
-    }
-
-    public void setEmail(String email) { 
-        this.email = email; 
-    }
-
-    public String getPassword() { 
-        return password; 
-    }
-
-    public void setPassword(String password) { 
-        this.password = password; 
-    }
-
-    public Set<Role> getRoles() { 
-        return roles; 
-    }
-
-    public void setRoles(Set<Role> roles) { 
-        this.roles = roles; 
-    }
-
-    public Boolean getActive() { 
-        return active; 
-    }
-
-    public void setActive(Boolean active) { 
-        this.active = active; 
-    }
+    private Boolean active;
 }
