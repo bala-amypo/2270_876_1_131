7f87267d2544f143e36a9c4f4a9138109a8b1f59
bala-amypo <bala@amypo.in>
2025-12-22 08:05:30 +0000

    User commit

--- src/main/java/com/example/demo/dto/ZoneDTO.java
+++ src/main/java/com/example/demo/dto/ZoneDTO.java
@@ -1,13 +1,19 @@
 package com.example.demo.dto;
 
 import lombok.*;
+import java.time.Instant;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class AuthRequest {
-    private String email;
-    private String password;
+public class ZoneDTO {
+    private Long id;
+    private String zoneName;
+    private Integer priorityLevel;
+    private Integer population;
+    private Boolean active;
+    private Instant createdAt;
+    private Instant updatedAt;
 }
