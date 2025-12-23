af712f496e3fdf032a86957acf8f7a073381a872
bala-amypo <bala@amypo.in>
2025-12-23 03:39:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoadSheddingEvent.java
+++ src/main/java/com/example/demo/entity/LoadSheddingEvent.java
@@ -1,15 +1,25 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class LoadSheddingEvent {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String zone;
-    private String reason;
+    private LocalDateTime eventStart;
 
     public LoadSheddingEvent() {}
+
+    public String getZone() {
+        return zone;
+    }
+
+    public LocalDateTime getEventStart() {
+        return eventStart;
+    }
 }
