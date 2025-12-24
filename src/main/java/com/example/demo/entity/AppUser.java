c28584534847de8510384b21ffad6fb959ec5ac3
bala-amypo <bala@amypo.in>
2025-12-24 04:31:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -1,7 +1,6 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -10,10 +9,24 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "app_users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
 public class AppUser {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
-    private String role;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder.Default
+    private Boolean active = true;
 }
